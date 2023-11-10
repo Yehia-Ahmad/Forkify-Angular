b0c924586b7304d5ea119fa44a51058529c900ba
Yehia-Ahmed <yehia790200@gmail.com>
2023-11-10 19:39:33 +0200

    UPDATE

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import recipe from '../../../recipe.json';
 
 @Component({
   selector: 'app-recipe-page',
   templateUrl: './recipe-page.component.html',
-  styleUrls: ['./recipe-page.component.css']
+  styleUrls: ['./recipe-page.component.css'],
 })
 export class RecipePageComponent {
+  ingredients: Object[] = recipe.data.recipe.ingredients;
+  title: string = recipe.data.recipe.title;
+  publisher: string = recipe.data.recipe.publisher;
+  imageUrl: string = recipe.data.recipe.image_url;
+  sourceUrl: string = recipe.data.recipe.source_url;
+  servings: number = recipe.data.recipe.servings;
+  cookingTime: number = recipe.data.recipe.cooking_time;
 
+  constructor() {}
 }
