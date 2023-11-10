d3de274d1d8d8604cd7f64b129e9dda3eb6557ce
Yehia-Ahmed <yehia790200@gmail.com>
2023-11-10 22:22:45 +0200

    UPDATE

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import recipe from '../../../recipe.json';
+// import recipeData from '../../../recipe.json';
 
 @Component({
   selector: 'app-recipe-page',
@@ -7,7 +8,7 @@ import recipe from '../../../recipe.json';
   styleUrls: ['./recipe-page.component.css'],
 })
 export class RecipePageComponent {
-  ingredients: Object[] = recipe.data.recipe.ingredients;
+  ingredients: any = recipe.data.recipe.ingredients;
   title: string = recipe.data.recipe.title;
   publisher: string = recipe.data.recipe.publisher;
   imageUrl: string = recipe.data.recipe.image_url;
@@ -15,5 +16,31 @@ export class RecipePageComponent {
   servings: number = recipe.data.recipe.servings;
   cookingTime: number = recipe.data.recipe.cooking_time;
 
-  constructor() {}
+  // recipe: object = {
+  //   ingredients: recipeData.data.recipe.ingredients,
+  //   title: recipeData.data.recipe.title,
+  //   publisher: recipeData.data.recipe.publisher,
+  //   imageUrl: recipeData.data.recipe.image_url,
+  //   sourceUrl: recipeData.data.recipe.source_url,
+  //   servings: recipeData.data.recipe.servings,
+  //   cookingTime: recipeData.data.recipe.cooking_time,
+  // };
+
+  constructor() {
+    // this.recipe = {
+    //   ingredients: recipeData.data.recipe.ingredients,
+    //   title: recipeData.data.recipe.title,
+    //   publisher: recipeData.data.recipe.publisher,
+    //   imageUrl: recipeData.data.recipe.image_url,
+    //   sourceUrl: recipeData.data.recipe.source_url,
+    //   servings: recipeData.data.recipe.servings,
+    //   cookingTime: recipeData.data.recipe.cooking_time,
+    // };
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.ingredients);
+  }
 }
