af767a52bb0cd87e4088fca18628111006b2c2e4
Yehia-Ahmed <yehia790200@gmail.com>
2023-11-11 00:20:03 +0200

    UPDATE

---
+++ src/app/Pipe/recipe-quantity.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RecipeQuantityPipe } from './recipe-quantity.pipe';
+
+describe('RecipeQuantityPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RecipeQuantityPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
