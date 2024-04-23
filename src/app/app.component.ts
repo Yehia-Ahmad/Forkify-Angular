2da74b4b5dcf08ecc451d8f84077487a1a8d9887
Yehia <Yehia>
2024-04-23 11:26:15 +0200

    Update Scroll-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   displayOverlyModel: boolean = false;
+  isChrome: boolean = true;
+
+  ngOnInit(): void {
+    this.isChrome = navigator.userAgent.includes("Chrome");
+  }
 
   showOverlyModel($event: boolean) {
     this.displayOverlyModel = true;
